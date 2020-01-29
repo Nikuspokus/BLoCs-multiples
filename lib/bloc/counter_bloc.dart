3c801ecfdab2a197e4133174d02d0117bb57d47f
marquillier <nicolas.marquillier@gmail.com>
2020-01-29 11:37:31 +0100

    15 CounterBloc 2

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,9 +1,21 @@
+import 'dart:async';
+
 import 'bloc.dart';
 
 class CounterBloc extends Bloc {
+  int count = 0;
+  final _streamController = StreamController<int>();
+  Sink<int> get sink => _streamController.sink;
+  Stream<int> get stream => _streamController.stream;
+
+  increment() => sink.add(count++);
+
+  decrement() => sink.add(count--);
+
+  CounterBloc() {
+    sink.add(count);
+  }
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-  }
+  void dispose() => _streamController.close();
 }
--- lib/ui/counter_screen.dart
+++ lib/ui/counter_screen.dart
@@ -1,13 +1,23 @@
+import 'package:bloc_multi/bloc/bloc_provider.dart';
+import 'package:bloc_multi/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
 
 class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final bloc = BlocProvider .of<CounterBloc>(context);
     return Scaffold(
       appBar: AppBar(title: Text("Compteur")),
-      body: Center(child: Text("Compteur"),),
+      body: StreamBuilder(
+        stream: bloc.stream,
+        builder: (context, snapshot){
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+          );
+        },
+      ),
     );
   }
 }
